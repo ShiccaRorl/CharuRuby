1115b31665c51db77a711c9276ae3bd54f104ff8
ShiccaRorl <shicca.rorl@gmail.com>
2018-04-25 14:15:46 +0900

    template.rb

--- lib/Charu/template.rb
+++ lib/Charu/template.rb
@@ -1,4 +1,53 @@
 # -*- encoding: utf-8 -*-
 
 header = ""
-header = ''
+header = '
+<!doctype html>
+<html><head>
+  <meta charset = "utf-8">
+  <meta name="name" content="content">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <meta http-equiv="Content-Script-Type" content="text/javascript">
+  <meta http-equiv="Last-Modified" content="<%= Time.now().strftime("%a, %d %b %Y %H:%M:%S") %> GMT">
+  <meta name="keywords" content="<%= @keyword %> ">
+  <meta name="description" content="<%= @description %>">
+  	<link rel="stylesheet" href="theme/base.css" type="text/css" media="all">
+	<link rel="stylesheet" href="<%= @css_theme_path %>" title="<%= @home_title %>" type="text/css" media="all">
+  <title><%= @home_title %></title>
+
+    <style type="text/css">
+
+    </style>
+
+</head>
+
+<body>
+<a href="<%= @top_home_page %>"><h1><%= @home_title %></h1></a>
+<br>
+'
+
+body = ""
+body = '
+<% i = 0 %>
+<% @daydata.each{|day| %>
+<% if i == 0 then %>
+<h1><%= day.get_Datetime().strftime("%Y年%m月%d日 (%A)") %></h1>
+<% i = 1 %>
+<% end %>
+<% } %>
+
+<% @daydata.each{|day| %>
+<h2><span class="title"><br>
+<%= day.get_tile() %></h2>
+
+<p><%= day.get_log() %></p>
+<% } %>
+'
+
+footer = ""
+footer = '
+</body>
+<br>
+<%= Time.now.strftime("%Y-%m-%d %H:%M:%S (%A)") %>
+</html>
+'
