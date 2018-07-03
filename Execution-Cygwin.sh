bfce82f93a266a1c34247a2e0edd6e2981d8a5d5
ShiccaRorl <shicca.rorl@gmail.com>
2018-07-03 14:15:17 +0900

    実行コード追加。

---
+++ Execution-Cygwin.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+ 
+#作成日時:2018-05-31
+
+ruby.exe ./bin/bundle exec Charu
---
+++ Execution-Windows.cmd
@@ -0,0 +1,7 @@
+rem call copy C:\Users\ban\Dropbox\保存ファイル\プログラム\Xyzzy\cl\ChangeLogMemo-Private .\..\
+rem call cd Charu
+call bundle exec Charu > log.txt
+rem call bundle exec Charu ./../test.txt
+
+rem call bundle exec Charu ./../ChangeLogMemo-Private
+pause
